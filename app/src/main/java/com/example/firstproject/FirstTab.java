214a2ecaabfda31f5e5ffa34764fbfe46545188d
qsenn <qsenn1020@gmail.com>
2021-12-30 17:57:16 +0900

    tab1 basic updated

--- app/src/main/java/com/example/firstproject/FirstTab.java
+++ app/src/main/java/com/example/firstproject/FirstTab.java
@@ -1,12 +1,23 @@
 package com.example.firstproject;
 
+import android.content.res.AssetManager;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import com.google.gson.Gson;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.io.InputStream;
+import java.util.ArrayList;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -46,6 +57,12 @@ public class FirstTab extends Fragment {
         return fragment;
     }
 
+    private RecyclerView recyclerView;
+    private FirstTabAdapter adapter;
+    private ArrayList<ContactsVO> list = new ArrayList<>();
+
+    Gson gson = new Gson();
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -53,12 +70,56 @@ public class FirstTab extends Fragment {
             mParam1 = getArguments().getString(ARG_PARAM1);
             mParam2 = getArguments().getString(ARG_PARAM2);
         }
+
+
+
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        ViewGroup rootView = (ViewGroup) inflater.inflate(R.layout.fragment_first_tab, container, false);
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_first_tab, container, false);
+        recyclerView = (RecyclerView) rootView.findViewById(R.id.recyclerview);
+
+        list = getContactList();
+        adapter = new FirstTabAdapter(getActivity(), list);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+        recyclerView.setAdapter(adapter);
+
+        return rootView;
+    }
+
+
+    public ArrayList<ContactsVO> getContactList() {
+        ArrayList<ContactsVO> list_contacts = new ArrayList<>();
+        Gson gson = new Gson();
+        AssetManager assetManager = getResources().getAssets();
+
+        try {
+            InputStream is = assetManager.open("contacts.json");
+            byte[] buffer = new byte[is.available()];
+            is.read(buffer);
+            is.close();
+            String json = new String(buffer, "UTF-8");
+
+            JSONObject jsonObject = new JSONObject(json);
+
+            JSONArray jsonArray = jsonObject.getJSONArray("contacts");
+
+            int index = 0;
+            while (index < jsonArray.length()) {
+
+                ContactsVO contactsVO = gson.fromJson(jsonArray.get(index).toString(), ContactsVO.class);
+
+                list_contacts.add(contactsVO);
+
+                index++;
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return list_contacts;
     }
 }
---
+++ app/src/main/java/com/example/firstproject/FirstTabAdapter.java
@@ -0,0 +1,59 @@
+package com.example.firstproject;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.FragmentActivity;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+public class FirstTabAdapter extends RecyclerView.Adapter<FirstTabAdapter.Holder> {
+
+    private Context context;
+    private List<ContactsVO> list = new ArrayList<>();
+
+
+    public FirstTabAdapter(Context context, List<ContactsVO> list) {
+        this.context = context;
+        this.list = list;
+    }
+    @NonNull
+    @Override
+    public Holder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.first_tab_item_view, parent, false);
+        Holder holder = new Holder(view);
+        return holder;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull Holder holder, int position) {
+
+        int itemposition = position;
+        holder.name.setText(list.get(itemposition).name);
+        holder.number.setText(list.get(itemposition).number);
+    }
+
+    @Override
+    public int getItemCount() {
+        return list.size();
+    }
+
+    public class Holder extends RecyclerView.ViewHolder{
+        public TextView name;
+        public TextView number;
+
+        public Holder(View view){
+            super(view);
+            name = (TextView) view.findViewById(R.id.name);
+            number = (TextView) view.findViewById(R.id.number);
+        }
+    }
+}
